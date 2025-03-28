3a1cde7b5d33d9347bbf125ed689d38a5b6cfe18
joel_kennerley <joel_kennerley@hotmail.com>
2025-03-28 16:17:18 +1300

    created functions for finding the actual fight pages from the home page and the card pages.

--- ufc_scraping.py
+++ ufc_scraping.py
@@ -1,17 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 ufc_stats = "http://ufcstats.com/statistics/events/completed"
 
-result = requests.get(ufc_stats)
-soup = BeautifulSoup(result.content, 'html.parser')
-
 # scraping ufcstats.com to give links to every documented fight card
-s = soup.find('tbody')
-cards = s.find_all('a')
-links = []
-for card in cards:
-    links.append(card['href'])
+def card_finder(url):
+    result = requests.get(url)
+    soup = BeautifulSoup(result.content, 'html.parser')
+    s = soup.find('tbody')
+    cards = s.find_all('a')
+    card_links = []
+    for card in cards:
+        card_links.append(card['href'])
+    return card_links
+
+# retrieving each fight link in each card
+def fights(links):
+    fights_on_card = []
+    for link in links[1:2]:
+        fight = requests.get(link)
+        card_soup = BeautifulSoup(fight.content, 'html.parser')
+        fight_body_html = card_soup.find('tbody', class_='b-fight-details__table-body')
+        fight_links = fight_body_html.find_all('tr')
+        for fights in fight_links:
+            fights_on_card.append(fights['data-link'])
+    return fights_on_card
 
 
-
+print(fights(card_finder(ufc_stats)))

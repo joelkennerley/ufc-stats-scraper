71c0c9597de7f89ba1d6b9e08f3ed2355cd8e625
joel_kennerley <joel_kennerley@hotmail.com>
2025-04-03 14:51:57 +1300

    changed some variable names, created fightID variable.
    checkpoint before I try to add the sig strikes round by round aswell as the totals.

--- ufc_scraping.py
+++ ufc_scraping.py
@@ -18,44 +18,45 @@ def card_finder(url):
     return card_links
 
 # retrieving each fight link in each card
-def fights(links):
-    fights_on_card = []
-    for link in links[1:2]:
-        fight = requests.get(link)
-        card_soup = BeautifulSoup(fight.content, 'html.parser')
+def fights(card_urls):
+    all_fights_links = []
+    for link in card_urls[1:2]:
+        card = requests.get(link)
+        card_soup = BeautifulSoup(card.content, 'html.parser')
         fight_element = card_soup.find('tbody', class_='b-fight-details__table-body')
-        fight_links = fight_element.find_all('tr')
-        for fights in fight_links:
-            fights_on_card.append(fights['data-link'])
-    return fights_on_card
+        fight_rows = fight_element.find_all('tr')
+        for fights in fight_rows:
+            all_fights_links.append(fights['data-link'])
+    return all_fights_links
 
-# pretty cooked rn dont know what p is
-def get_stats(urls):
-    for url in urls:
+# retrieve stats for each round in a fight for every fight
+def get_stats(fight_urls):
+    for fight_ID, url in enumerate(fight_urls):
         fight = requests.get(url)
         fight_soup = BeautifulSoup(fight.content, 'html.parser')
-        fight_element = fight_soup.find('table', class_="b-fight-details__table js-fight-table")
 
         # retrieves html for each round
         fight_table = fight_soup.find("table", class_="b-fight-details__table")
-        fight_rows = fight_table.find_all("tr", class_="b-fight-details__table-row")
+        round_rows = fight_table.find_all("tr", class_="b-fight-details__table-row")
 
         round_stats = {}
         # stats are put into a dict with key=round, and values = name, sig str...
-        for i, row in enumerate(fight_rows):
-            f1 = []
-            f2 = []
+        for round_no, row in enumerate(round_rows):
+            f1 = [fight_ID, round_no]
+            f2 = [fight_ID, round_no]
             for td in row.find_all('td'):
                 p = td.find_all('p')
                 if len(p) >= 2:
                     f1.append(p[0].get_text(strip=True))
                     f2.append(p[1].get_text(strip=True))
-            round_stats[i] = [f1, f2]
+            round_stats[round_no] = [f1, f2]
         return round_stats
 
 def main():
-    something = fights(card_finder(ufc_stats))
-    print(get_stats(something))
+    fight_cards = card_finder(ufc_stats)
+    fight_links = fights(fight_cards)
+    fight_stats = get_stats(fight_links)
+    print(fight_stats)
 
 if __name__ == "__main__":
     main()
